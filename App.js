7c28bf2ee1c8020ab0b0f14f972d530227ac071c
raunak <raunak.kumar.20205@gmail.com>
2019-06-15 12:41:40 +0530

    updats in app.js

--- App.js
+++ App.js
@@ -24,7 +24,9 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+//raunak is here
+      
+</View>
     );
   }
 }
