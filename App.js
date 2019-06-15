e28c323215a4d266561cae14a91e40240836a2fe
raunak <raunak.kumar.20205@gmail.com>
2019-06-15 12:50:42 +0530

    updats in app.js

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default class App extends Component<Props> {
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
 //raunak is here
-      
+//new update 
 </View>
     );
   }
